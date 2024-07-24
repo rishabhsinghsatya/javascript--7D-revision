644b7ef0da7285a8e72389536b3eec89b8cfabc7
Rishabh Singh <vineetsatya36@gmail.com>
2024-07-24 11:28:06 +0530

    Prototypal inheritance, Object.create, classes (ES6)

---
+++ Day2_Advanced_Functions_Objects/prototypes.js
@@ -0,0 +1,26 @@
+function Animal(name) {
+  this.name = name;
+}
+
+Animal.prototype.speak = function () {
+  console.log(`${this.name} makes a noise.`);
+};
+
+const dog = new Animal("Dog");
+dog.speak(); // Output: Dog makes a noise.
+
+// Inheritance
+function Dog(name, breed) {
+  Animal.call(this, name);
+  this.breed = breed;
+}
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.speak = function () {
+  console.log(`${this.name} barks.`);
+};
+
+const myDog = new Dog("Buddy", "Golden Retriever");
+myDog.speak(); // Output: Buddy barks.
