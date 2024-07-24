13bc33c41a3706256be5ffb2c570c784d718c55e
Rishabh Singh <vineetsatya36@gmail.com>
2024-07-24 13:09:33 +0530

    add another example for closure

--- Day2_Advanced_Functions_Objects/closures.js
+++ Day2_Advanced_Functions_Objects/closures.js
@@ -10,3 +10,17 @@ const counter = createCounter();
 console.log(counter()); // Output: 1
 console.log(counter()); // Output: 2
 console.log(counter()); // Output: 3
+
+//another
+function outer() {
+  let name = "javascript";
+  function inner() {
+    let game = "game from inner function";
+    name = `${name} access from outer function in inner function() `;
+
+    console.log(game); // game from inner function
+    console.log(name); // javascript access from outer function in inner function()
+  }
+  inner();
+}
+outer();
