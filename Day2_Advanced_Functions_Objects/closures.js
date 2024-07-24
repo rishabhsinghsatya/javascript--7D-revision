e484114a27837504856cd168ff848874aef1dced
Rishabh Singh <vineetsatya36@gmail.com>
2024-07-24 11:25:14 +0530

    Understanding and using closures

---
+++ Day2_Advanced_Functions_Objects/closures.js
@@ -0,0 +1,12 @@
+function createCounter() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+const counter = createCounter();
+console.log(counter()); // Output: 1
+console.log(counter()); // Output: 2
+console.log(counter()); // Output: 3
