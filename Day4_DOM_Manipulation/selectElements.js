542fe0c3e6208a12032d2a7144f44633f11c407b
Rishabh Singh <vineetsatya36@gmail.com>
2024-07-26 18:55:40 +0530

    getElementById, querySelector, etc.

---
+++ Day4_DOM_Manipulation/selectElements.js
@@ -0,0 +1,18 @@
+// Select by ID
+const header = document.getElementById("header");
+console.log(header);
+
+// Select by Class
+const buttons = document.getElementsByClassName("button");
+console.log(buttons);
+
+// Select by Tag Name
+const paragraphs = document.getElementsByTagName("p");
+console.log(paragraphs);
+
+// Select by CSS Selector
+const firstButton = document.querySelector(".button");
+console.log(firstButton);
+
+const allButtons = document.querySelectorAll(".button");
+console.log(allButtons);
