e5c483e69b77643382f451eece4b11d0a401ad9c
Rishabh Singh <vineetsatya36@gmail.com>
2024-07-26 18:57:04 +0530

    Creating, updating, deleting elements

---
+++ Day4_DOM_Manipulation/modifyElements.js
@@ -0,0 +1,12 @@
+// Change Text Content
+const header = document.getElementById("header");
+header.textContent = "New Header Text";
+
+// Change HTML Content
+const container = document.querySelector(".container");
+container.innerHTML += "<p>New content added!</p>"; // Added new content
+
+// Change Styles
+const button = document.querySelector(".button");
+button.style.backgroundColor = "blue";
+button.style.color = "white";
